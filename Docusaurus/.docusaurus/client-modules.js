d3e9f8a07d95c4d10cd1a8e748690dde51a4a651
alifismady <alifismady@gmail.com>
2023-05-14 15:46:38 +0700

    deployment

---
+++ Docusaurus/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/alifismady/git/231saurus/Docusaurus/node_modules/infima/dist/css/default/default.css'),
+  require('/home/alifismady/git/231saurus/Docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/alifismady/git/231saurus/Docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/alifismady/git/231saurus/Docusaurus/src/css/custom.css'),
+];
